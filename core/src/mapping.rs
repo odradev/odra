8802af9f92be0d17ad7013ef5b3e4e04d29ad3a9
Krzysztof Pobiarżyn <krzysztof.pobiarzyn@gmail.com>
2022-06-21 10:33:20 +0200

    add Mapping implementation

---
+++ core/src/mapping.rs
@@ -0,0 +1,83 @@
+use std::{
+    hash::Hash,
+    marker::PhantomData,
+    ops::{Add, Sub},
+};
+
+use crate::Env;
+use odra_types::{
+    bytesrepr::{FromBytes, ToBytes},
+    CLTyped,
+};
+
+use crate::instance::Instance;
+
+#[derive(Debug)]
+pub struct Mapping<K, V> {
+    name: String,
+    key_ty: PhantomData<K>,
+    value_ty: PhantomData<V>,
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped> Mapping<K, V> {
+    pub fn new(name: String) -> Self {
+        Mapping {
+            name,
+            key_ty: PhantomData::<K>::default(),
+            value_ty: PhantomData::<V>::default(),
+        }
+    }
+
+    pub fn get(&self, key: &K) -> Option<V> {
+        let result = Env::get_dict_value(&self.name, key);
+
+        if let Some(value) = result {
+            return Some(value.into_t::<V>().unwrap());
+        }
+        None
+    }
+
+    pub fn set(&self, key: &K, value: V) {
+        Env::set_dict_value(&self.name, key, value);
+    }
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped + Default> Mapping<K, V> {
+    pub fn get_or_default(&self, key: &K) -> V {
+        self.get(key).unwrap_or_default()
+    }
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped + Add<Output = V> + Default>
+    Mapping<K, V>
+{
+    pub fn add(&self, key: &K, value: V) {
+        let current_value = self.get(key).unwrap_or_default();
+        // TODO: check overflow
+        let new_value = current_value + value;
+        Env::set_dict_value(&self.name, key, new_value);
+    }
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped + Sub<Output = V> + Default>
+    Mapping<K, V>
+{
+    pub fn subtract(&self, key: &K, value: V) {
+        let current_value = self.get(key).unwrap_or_default();
+        // TODO: check overflow
+        let new_value = current_value - value;
+        Env::set_dict_value(&self.name, key, new_value);
+    }
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped> From<&str> for Mapping<K, V> {
+    fn from(name: &str) -> Self {
+        Mapping::new(name.to_string())
+    }
+}
+
+impl<K: ToBytes + CLTyped + Hash, V: ToBytes + FromBytes + CLTyped> Instance for Mapping<K, V> {
+    fn instance(namespace: &str) -> Self {
+        namespace.into()
+    }
+}
