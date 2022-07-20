392a7303630a6c6b8415962baa6d6c9c8c770d7e
zie1ony <maciej@odra.dev>
2022-07-20 22:01:10 +0200

    Fixed assert_events macro.

--- core/src/test_utils.rs
+++ core/src/test_utils.rs
@@ -14,9 +14,13 @@ where
 #[macro_export]
 macro_rules! assert_events {
     ($contract:ident, $ ( $event_ty:ty ),+ ) => {
-        let mut __idx = 0;
+        let mut __idx = -1;
         $(
             __idx -= 1;
+            let _ = $event_ty;
+        )+
+        $(
+            __idx += 1;
             let ev = odra::test_utils::get_event::<$event_ty>(&$contract.address(), __idx).unwrap();
             assert_eq!(
                 <$event_ty as odra::types::event::Event>::name(&ev), stringify!($event_ty).to_string()
@@ -24,9 +28,13 @@ macro_rules! assert_events {
         )+
     };
     ($contract:ident, $ ( $event:expr ),+ ) => {
-        let mut __idx = 0;
+        let mut __idx = -1;
         $(
             __idx -= 1;
+            let _ = $event;
+        )+
+        $(
+            __idx += 1;
             let ev = odra::test_utils::get_event(&$contract.address(), __idx).unwrap();
             assert_eq!(
                 $event, ev
