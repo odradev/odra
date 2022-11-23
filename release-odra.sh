38aa45ffbe4d08c1b81e38538729f6c3a1b8595d
zie1ony <maciej@odra.dev>
2022-11-23 15:43:48 +0100

    Add versions to Cargo.toml files.

---
+++ release-odra.sh
@@ -0,0 +1,14 @@
+# cargo publish -p odra-types && sleep 100
+# cargo publish -p odra-utils && sleep 100
+# cargo publish -p odra-mock-vm-types && sleep 100
+# cargo publish -p odra-mock-vm && sleep 100
+# cargo publish -p odra-ir && sleep 100
+# cargo publish -p odra-codegen && sleep 100
+# cargo publish -p odra-proc-macros --allow-dirty && sleep 100
+cargo publish -p odra-casper-shared && sleep 100
+cargo publish -p odra-casper-types && sleep 100
+cargo publish -p odra-casper-test-env && sleep 100
+cargo publish -p odra-casper-codegen && sleep 100
+cargo publish -p odra-casper-backend && sleep 100
+cargo publish -p odra && sleep 100
+# cd modules && cargo publish
