42d588ce8865334a44227cc9dd14ac7c7b745f4c
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2023-03-23 16:50:26 +0100

    Increase reliability of recordings and completions
    - Remove `mock` module from `access.rs` file
    
    [modules/src/access.rs]
    - Remove `mock` module from `access.rs` file

--- modules/src/access.rs
+++ modules/src/access.rs
@@ -7,7 +7,3 @@ pub use access_control::{
     AccessControl, AccessControlDeployer, AccessControlRef, Role, DEFAULT_ADMIN_ROLE
 };
 pub use ownable::*;
-
-pub mod mock {
-    pub use super::access_control::mock::*;
-}
