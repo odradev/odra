bbb2628ad2175f137e08292110a0647bc308bc85
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2023-04-24 14:27:53 +0200

    update macro to accept tuples

--- mock-vm/types/src/ty.rs
+++ mock-vm/types/src/ty.rs
@@ -8,7 +8,7 @@ pub trait Typed {
 }
 
 macro_rules! impl_typed {
-    ($($ty:ty, $tt:expr),*) => {
+    ($(($ty:ty, $tt:expr)),*) => {
         $(
             impl Typed for $ty {
                 fn ty() -> Type {
@@ -20,32 +20,19 @@ macro_rules! impl_typed {
 }
 
 impl_typed!(
-    i32,
-    Type::I32,
-    i64,
-    Type::I64,
-    u8,
-    Type::U8,
-    u32,
-    Type::U32,
-    u64,
-    Type::U64,
-    U128,
-    Type::U128,
-    U256,
-    Type::U256,
-    U512,
-    Type::U512,
-    bool,
-    Type::Bool,
-    (),
-    Type::Unit,
-    String,
-    Type::String,
-    Address,
-    Type::Address,
-    Bytes,
-    Type::Vec(Box::new(Type::U8))
+    (i32, Type::I32),
+    (i64, Type::I64),
+    (u8, Type::U8),
+    (u32, Type::U32),
+    (u64, Type::U64),
+    (U128, Type::U128),
+    (U256, Type::U256),
+    (U512, Type::U512),
+    (bool, Type::Bool),
+    ((), Type::Unit),
+    (String, Type::String),
+    (Address, Type::Address),
+    (Bytes, Type::Vec(Box::new(Type::U8)))
 );
 
 macro_rules! impl_typed_for_array {
