8e56219f019bd31e2e0559c4c964bfd23f5d5e99
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2023-07-13 21:06:19 +0200

    [HAL-02] namespace collision (#204)
    * Add a utility function to create a storage key.
    * Verify the correctness of a struct fields

--- lang/ir/src/utils.rs
+++ lang/ir/src/utils.rs
@@ -1,4 +1,7 @@
-use syn::{Data, DataStruct, DeriveInput, Fields, FieldsNamed};
+use syn::{
+    visit::{self, Visit},
+    Data, DataStruct, DeriveInput, Fields, FieldsNamed
+};
 
 pub fn extract_fields(input: DeriveInput) -> Result<FieldsNamed, syn::Error> {
     let fields = match input.data {
@@ -14,4 +17,34 @@ pub fn extract_fields(input: DeriveInput) -> Result<FieldsNamed, syn::Error> {
         }
     };
     Ok(fields)
+}
+
+pub struct FieldsValidator(Result<(), syn::Error>);
+
+impl From<&syn::ItemStruct> for FieldsValidator {
+    fn from(value: &syn::ItemStruct) -> Self {
+        let mut visitor = FieldsValidator(Ok(()));
+        visitor.visit_item_struct(value);
+        visitor
+    }
+}
+
+impl FieldsValidator {
+    pub fn result(self) -> Result<(), syn::Error> {
+        self.0
+    }
+}
+
+impl<'ast> Visit<'ast> for FieldsValidator {
+    fn visit_field(&mut self, f: &'ast syn::Field) {
+        if let Some(f) = &f.ident {
+            if f.to_string().contains(odra_utils::KEY_DELIMITER) {
+                self.0 = Err(syn::Error::new_spanned(
+                    f,
+                    "Invalid character '#' in the field name"
+                ))
+            }
+        }
+        visit::visit_field(self, f);
+    }
 }
