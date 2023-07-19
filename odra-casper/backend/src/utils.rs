8e7d53f442c0b79018f254b035b5422450e4cb75
Maciej Zieliński <maciej@odra.dev>
2023-07-19 11:21:53 +0200

    Smarter Casper purses. (#210)
    Smarter Casper purses.

--- odra-casper/backend/src/utils.rs
+++ odra-casper/backend/src/utils.rs
@@ -31,8 +31,9 @@ pub fn named_arg_exists(name: &str) -> bool {
 }
 
 /// Gets the currently executing contract main purse [URef].
-pub fn get_main_purse() -> URef {
-    casper_env::get_or_create_purse()
+/// It creates a new purse if it doesn't exist.
+pub fn get_or_create_main_purse() -> URef {
+    casper_env::get_or_create_main_purse()
 }
 
 /// Stores in memory the amount attached to the current call.
@@ -59,7 +60,7 @@ pub fn handle_attached_value() {
     let cargo_purse = runtime::get_named_arg(consts::CARGO_PURSE_ARG);
     let amount = system::get_purse_balance(cargo_purse);
     if let Some(amount) = amount {
-        let contract_purse = get_main_purse();
+        let contract_purse = get_or_create_main_purse();
         system::transfer_from_purse_to_purse(cargo_purse, contract_purse, amount, None)
             .unwrap_or_revert();
         set_attached_value(amount.into());
