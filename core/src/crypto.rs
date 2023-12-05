9afdb941bf25504b073c18d635a401cb5613964f
Kuba Płaskonka <kuba@odra.dev>
2023-12-05 10:38:39 +0100

    Another round of PR fixes

--- core/src/crypto.rs
+++ core/src/crypto.rs
@@ -7,16 +7,20 @@ pub fn generate_key_pairs(amount: u8) -> BTreeMap<Address, (SecretKey, PublicKey
     let mut accounts = BTreeMap::new();
     for i in 0..amount {
         // Create keypair.
-        let secret_key = SecretKey::ed25519_from_bytes([i; 32]).unwrap();
+        let secret_key = SecretKey::ed25519_from_bytes([i; 32]).unwrap_or_else(|_| {
+            panic!(
+                "Couldn't construct a secret key from {}. This shouldn't happen!",
+                i
+            )
+        });
         let public_key = PublicKey::from(&secret_key);
 
         // Create an AccountHash from a public key.
         let account_addr = AccountHash::from(&public_key);
 
-        let address = match account_addr.try_into() {
-            Ok(address) => address,
-            Err(_) => continue
-        };
+        let address = account_addr.try_into().unwrap_or_else(|_| {
+            panic!("Couldn't convert AccountHash to Address. This shouldn't happen!")
+        });
 
         // Create a GenesisAccount.
         accounts.insert(address, (secret_key, public_key));
