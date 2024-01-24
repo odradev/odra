e1e52d6fe9aa5d8877113b9bc2784abd6669762e
Kuba Płaskonka <kuba@odra.dev>
2024-01-24 13:47:54 +0100

    Message signing

--- examples/bin/livenet_tests.rs
+++ examples/bin/livenet_tests.rs
@@ -18,7 +18,9 @@ fn main() {
     // let mut contract = load(&env, *contract.address());
 
     // Uncomment to load existing contract
-    let address = Address::from_str("hash-bd80e12b6d189e3b492932fc08e1342b540555c60e299ea3563d81ad700997e0").unwrap();
+    let address =
+        Address::from_str("hash-bd80e12b6d189e3b492932fc08e1342b540555c60e299ea3563d81ad700997e0")
+            .unwrap();
     let mut contract = load(&env, address);
 
     // Set gas will be used for all subsequent calls
@@ -46,9 +48,10 @@ fn main() {
     assert_eq!(contract.immutable_cross_call(), 10_000.into());
 
     // - mutable crosscalls will require a deploy
-    let pre_call_balance = Erc20Deployer::load(&env, erc20_address()).balance_of(env.caller());
+    let erc20 = Erc20Deployer::load(&env, erc20_address());
+    let pre_call_balance = erc20.balance_of(env.caller());
     contract.mutable_cross_call();
-    let post_call_balance = Erc20Deployer::load(&env, erc20_address()).balance_of(env.caller());
+    let post_call_balance = erc20.balance_of(env.caller());
     assert_eq!(post_call_balance, pre_call_balance + 1);
 
     // We can change the caller
