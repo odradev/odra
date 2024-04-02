ca3d8f8c4cdbd174332b2e33474c57edac32fbd6
Kuba Płaskonka <kuba.plaskonka@gmail.com>
2024-04-02 10:30:07 +0200

    Bump versions to 0.9.0. (#392)
    Update changelog dates.
    Added odra-schema to release script.

--- release-odra.sh
+++ release-odra.sh
@@ -8,6 +8,7 @@ cargo publish -p odra-core
 cargo publish -p odra-macros
 cargo publish -p odra-vm
 cargo publish -p odra-casper-wasm-env 
+cargo publish -p odra-schema
 cargo publish -p odra
 cargo publish -p odra-casper-test-vm
 cargo publish -p odra-test
