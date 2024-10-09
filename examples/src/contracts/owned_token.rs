bd20ae20c79cdeba1e2f7432c0e4372a1d634f82
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2024-10-09 15:16:30 +0200

    Add test of a manual `init` call

--- examples/src/contracts/owned_token.rs
+++ examples/src/contracts/owned_token.rs
@@ -51,7 +51,7 @@ impl OwnedToken {
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use odra::host::{Deployer, HostRef};
+    use odra::{host::{Deployer, HostRef}, VmError};
     use odra_modules::access::errors::Error::CallerNotTheOwner;
 
     pub const NAME: &str = "Plascoin";
@@ -93,6 +93,13 @@ pub mod tests {
                 amount: INITIAL_SUPPLY.into()
             }
         );
+    }
+
+    #[test]
+    fn should_not_init_twice() {
+        let mut token = setup();
+        let result = token.try_init(String::from(NAME), String::from(SYMBOL), 11u8, INITIAL_SUPPLY.into());
+        assert_eq!(result.unwrap_err(), OdraError::VmError(VmError::InvalidContext));
     }
 
     #[test]
