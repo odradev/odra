d411a36d66d2576a7ea63898f089a159762c4d9d
Kuba Płaskonka <kuba@odra.dev>
2025-03-18 14:16:14 +0100

    Conversion error

--- core/src/arithmetic.rs
+++ core/src/arithmetic.rs
@@ -20,7 +20,9 @@ pub enum ArithmeticsError {
     /// Addition result exceeds the max value.
     AdditionOverflow,
     /// Subtraction result is lower than the min value.
-    SubtractingOverflow
+    SubtractingOverflow,
+    /// Conversion error
+    ConversionError
 }
 
 /// Implements [OverflowingAdd] and [OverflowingSub] for all the given types.
