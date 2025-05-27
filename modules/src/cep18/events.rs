b4528b5ae54700ad7bd2bf18cf07bce1e9050cb7
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2025-05-27 11:17:03 +0200

    Refactor CEP-18 token module: remove security features and update tests

--- modules/src/cep18/events.rs
+++ modules/src/cep18/events.rs
@@ -1,8 +1,6 @@
 use odra::casper_types::U256;
 use odra::prelude::*;
 
-use crate::cep18::utils::SecurityBadge;
-
 /// An event emitted when a mint operation is performed.
 #[odra::event]
 pub struct Mint {
@@ -80,13 +78,4 @@ pub struct TransferFrom {
     pub recipient: Address,
     /// The amount of tokens transferred.
     pub amount: U256
-}
-
-/// An event emitted when a security rules change is performed.
-#[odra::event]
-pub struct ChangeSecurity {
-    /// The address of the administrator which perfomed the change.
-    pub admin: Address,
-    /// The map of changes made to the security rules.
-    pub sec_change_map: BTreeMap<Address, SecurityBadge>
 }
