73be2fbaf6d01410861f3f352fd506e246050094
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2025-06-06 09:03:19 +0200

    Add CLI support and deployment scripts for various templates

---
+++ templates/full/bin/cli.rs
@@ -0,0 +1,80 @@
+//! This example demonstrates how to use the `odra-cli` tool to deploy and interact with a smart contract.
+
+use {{project-name}}::flipper::Flipper;
+use odra::host::{HostEnv, NoArgs};
+use odra::schema::casper_contract_schema::NamedCLType;
+use odra_cli::{
+    deploy::DeployScript,
+    scenario::{Scenario, ScenarioMetadata},
+    CommandArg, DeployedContractsContainer, DeployerExt, OdraCli, ScenarioArgs, 
+    ScenarioError
+};
+
+/// Deploys the `Flipper` and adds it to the container.
+pub struct FlipperDeployScript;
+
+impl DeployScript for FlipperDeployScript {
+    fn deploy(
+        &self,
+        env: &HostEnv,
+        container: &mut DeployedContractsContainer
+    ) -> Result<(), odra_cli::deploy::Error> {
+        let _flipper = Flipper::load_or_deploy(
+            &env,
+            NoArgs,
+            container,
+            350_000_000_000 // Adjust gas limit as needed
+        )?;
+
+        Ok(())
+    }
+}
+
+/// Scenario that flips the state of the deployed `Flipper` contract a specified number of times.
+pub struct FlippingScenario;
+
+impl Scenario for FlippingScenario {
+    fn args(&self) -> Vec<CommandArg> {
+        vec![CommandArg::new(
+            "number",
+            "The number of times to flip the state",
+            NamedCLType::U64,
+            false,
+            false
+        )]
+    }
+
+    fn run(
+        &self,
+        env: &HostEnv,
+        container: DeployedContractsContainer,
+        args: ScenarioArgs
+    ) -> Result<(), ScenarioError> {
+        let mut contract = container.get_ref::<Flipper>(env)?;
+        let n = args.get_single::<u64>("name")?;
+
+        env.set_gas(50_000_000);
+        for _ in 0..n {
+            contract.try_flip()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl ScenarioMetadata for FlippingScenario {
+    const NAME: &'static str = "flip";
+    const DESCRIPTION: &'static str =
+        "Flips the state of the deployed flipper contract a specified number of times";
+}
+
+/// Main function to run the CLI tool.
+pub fn main() {
+    OdraCli::new()
+        .about("CLI tool for {{project-name}} smart contract")
+        .deploy(FlipperDeployScript)
+        .contract::<Flipper>()
+        .scenario(FlippingScenario)
+        .build()
+        .run();
+}
