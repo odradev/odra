24f442204c5130c5236e032b886b00b45cd26e74
Maciej Zieliński <maciej@odra.dev>
2025-06-17 15:20:19 +0200

    Enforce `balances` and `allowances` named keys in cep18

--- modules/src/cep18_token.rs
+++ modules/src/cep18_token.rs
@@ -33,20 +33,33 @@ impl Cep18 {
     /// Initializes the contract with the given metadata, initial supply.
     pub fn init(&mut self, symbol: String, name: String, decimals: u8, initial_supply: U256) {
         let caller = self.env().caller();
-        // set the metadata
+
+        // Set the metadata
         self.symbol.set(symbol);
         self.name.set(name);
         self.decimals.set(decimals);
         self.total_supply.set(initial_supply);
 
+        // Set the initial balance of the caller.
+        //
+        // NOTE: This is executed also if the initial supply is zero, so the
+        // `balances` named key is initialized.
+        self.balances.set(&caller, initial_supply);
+
+        // If the initial supply is not zero, emit a Mint event.
         if !initial_supply.is_zero() {
-            // mint the initial supply for the caller
-            self.balances.set(&caller, initial_supply);
             self.env().emit_event(Mint {
                 recipient: caller,
                 amount: initial_supply
             });
         }
+
+        // NOTE: Allow the current contract to spend zero tokens on behalf of
+        // the caller. Zero is the default value, so it doesn't really change
+        // anything. By doing so, the `allowances` named key is initialized,
+        // which is a good practice.
+        let self_address = self.env().self_address();
+        self.allowances.set(&self_address, &caller, U256::zero());
     }
 
     /// Returns the name of the token.
