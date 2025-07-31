d3ad655455c01e5a518ece5882c8b1c06cf43b8a
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2025-07-31 12:39:30 +0200

    Improve panic hook to enhance error reporting with location details and assertion messages

--- odra-vm/src/panic_hook.rs
+++ odra-vm/src/panic_hook.rs
@@ -5,26 +5,41 @@ const EXEC_PARTS_PREFIX: &str = "exec_parts::execute";
 
 pub fn set_odra_panic_hook() {
     std::panic::set_hook(Box::new(|panic_info| {
-        let panic_message = if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
-            *s
-        } else if let Some(s) = panic_info.payload().downcast_ref::<String>() {
-            s.as_str()
+        // let panic_message = if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+        //     *s
+        // } else if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+        //     s.as_str()
+        // } else {
+        //     "Unknown panic"
+        // };
+        let panic_message = if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+            s.clone()
+        } else if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+            s.to_string()
         } else {
-            "Unknown panic"
+            "Unknown panic".to_string()
         };
 
         let location = panic_info
             .location()
             .map(|l| format!("{}:{}", l.file(), l.line()))
             .unwrap_or_else(|| "<unknown>".into());
-        if let Some(contract) = extract_contract_address(panic_message) {
+
+        // Print the panic message if an assertion failed
+        if panic_message.contains("assertion") {
+            eprintln!("💥 {panic_message}");
+            eprintln!("    ↳ at {location}");
+        }
+
+        if let Some(contract) = extract_contract_address(&panic_message) {
             eprintln!("{contract}");
         }
+
         if panic_message.starts_with(REVERT_MESSAGE_PREFIX) {
-            if let Some(error_name) = extract_error_name(panic_message) {
+            if let Some(error_name) = extract_error_name(&panic_message) {
                 eprintln!("💣 {error_name}");
             } else {
-                eprintln!("💣 {panic_message} at {location}");
+                eprintln!("💣 {panic_message}");
             }
         }
         // Find the first symbol that contains `exec_parts::execute` in its name
@@ -38,29 +53,15 @@ pub fn set_odra_panic_hook() {
                         let name = name.to_string();
                         if name.contains(EXEC_PARTS_PREFIX) {
                             if let Some(prev) = prev_symbol {
-                                match (prev.name(), prev.filename(), prev.lineno()) {
-                                    (Some(prev_name), Some(prev_filename), Some(prev_lineno)) => {
-                                        let prev_name = prev_name.to_string();
-                                        let prev_name = prev_name
-                                            .rfind("::")
-                                            .map_or(prev_name.clone(), |pos| {
-                                                prev_name[..pos].to_string()
-                                            });
-                                        eprintln!("  ↳ {prev_name}");
-                                        eprintln!(
-                                            "    ↳ at {}:{prev_lineno}",
-                                            prev_filename.to_string_lossy()
-                                        );
-                                    }
-                                    _ => {} // no-op
-                                }
-                                return;
+                                print_symbol_with_location(prev);
+                                // return;
                             }
                         }
                         prev_symbol = Some(symbol.clone());
                     }
                     _ => {} // no-op
                 }
+                // eprintln!("  ↳ {}", symbol.name().unwrap());
             }
         }
     }));
@@ -84,4 +85,18 @@ fn extract_contract_address(panic_message: &str) -> Option<String> {
     let contract_idx = panic_message.find(CONTRACT_PREFIX)?;
     let contract_part = &panic_message[contract_idx..];
     Some(contract_part.to_string())
+}
+
+fn print_symbol_with_location(symbol: backtrace::BacktraceSymbol) {
+    match (symbol.name(), symbol.filename(), symbol.lineno()) {
+        (Some(name), Some(filename), Some(lineno)) => {
+            let name = name.to_string();
+            let name = name
+                .rfind("::")
+                .map_or(name.clone(), |pos| name[..pos].to_string());
+            eprintln!("  ↳ {name}");
+            eprintln!("    ↳ at {}:{lineno}", filename.to_string_lossy());
+        }
+        _ => {} // no-op
+    }
 }
