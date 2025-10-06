27743dc6d1a517eba3eba08ee6d769d7e8e25df2
Krzysztof Pobiarzyn <krzysztof@odra.dev>
2025-10-06 12:43:27 +0200

    fix: update IntoOdraValue methods to use into_odra_value

--- odra-wasm-client-builder/src/codegen/custom_types.rs
+++ odra-wasm-client-builder/src/codegen/custom_types.rs
@@ -220,7 +220,7 @@ fn enum_def(name: &str, variants: &[EnumVariant], description: String) -> TokenS
         }
 
         impl odra_wasm_client::types::IntoOdraValue<#type_name> for #type_name {
-            fn to_wasm_value(self) -> Result<#type_name, JsError> {
+            fn into_odra_value(self) -> Result<#type_name, JsError> {
                 Ok(self)
             }
         }
@@ -252,7 +252,7 @@ fn field_init(member: &StructMember) -> proc_macro2::TokenStream {
         return parse_quote!(#field_name);
     }
     quote::quote! {
-        #field_name: odra_wasm_client::types::IntoOdraValue::to_wasm_value(#field_name)?
+        #field_name: odra_wasm_client::types::IntoOdraValue::into_odra_value(#field_name)?
     }
 }
 
@@ -270,7 +270,7 @@ fn getter_code(member: &StructMember) -> proc_macro2::TokenStream {
     quote::quote! {
         #[wasm_bindgen(getter)]
         pub fn #ident(&self) -> #ty {
-            odra_wasm_client::types::IntoWasmValue::to_wasm_value(self.#ident.clone())
+            odra_wasm_client::types::IntoWasmValue::into_odra_value(self.#ident.clone())
         }
     }
 }
@@ -283,7 +283,7 @@ fn setter_code(member: &StructMember) -> proc_macro2::TokenStream {
     quote::quote! {
         #[wasm_bindgen(setter)]
         pub fn #ident(&mut self, value: #ty) {
-            self.#field_name = odra_wasm_client::types::IntoOdraValue::to_wasm_value(value).unwrap();
+            self.#field_name = odra_wasm_client::types::IntoOdraValue::into_odra_value(value).unwrap();
         }
     }
 }
