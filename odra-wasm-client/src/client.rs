aeaa650fc2e64f7a7ec5d93c01d1c198981a5312
Maciej Zieliński <maciej@odra.dev>
2026-01-26 14:15:32 +0100

    resources dir for odra-wasm-client

--- odra-wasm-client/src/client.rs
+++ odra-wasm-client/src/client.rs
@@ -37,7 +37,7 @@ const CHAIN_TESTNET: &str = "casper-test";
 const SECRET_KEY_PEM: &str = env!("WASM_CLIENT_SK");
 
 pub const PROXY_CALLER: &[u8; 52814] =
-    include_bytes!("../../resources/proxy_caller_with_return.wasm");
+    include_bytes!("../resources/proxy_caller_with_return.wasm");
 
 static GAS: OnceLock<Arc<Mutex<u64>>> = OnceLock::new();
 
